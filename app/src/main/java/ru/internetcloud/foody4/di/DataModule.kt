af5c0a6a8bba882145ea058296dc460851f9dc30
harisov-mobile <v8spec@mail.ru>
2022-08-01 18:09:27 +0300

    Add DI classes

---
+++ app/src/main/java/ru/internetcloud/foody4/di/DataModule.kt
@@ -0,0 +1,40 @@
+package ru.internetcloud.foody4.di
+
+import dagger.Module
+
+@Module
+interface DataModule {
+
+//    @ApplicationScope
+//    @Binds
+//    fun bindFoodFilterRepository(impl: FoodFilterRepositoryImpl): FoodFilterRepository
+//
+//    @ApplicationScope
+//    @Binds
+//    fun bindFoodRecipeRepository(impl: FoodRecipeRepositoryImpl): FoodRecipeRepository
+//
+//    @ApplicationScope
+//    @Binds
+//    fun bindFoodRecipeRemoteDataSource(impl: FoodRecipeRemoteDataSourceImpl): FoodRecipeRemoteDataSource
+//
+//    @ApplicationScope
+//    @Binds
+//    fun bindFoodRecipeLocalDataSource(impl: FoodRecipeLocalDataSourceImpl): FoodRecipeLocalDataSource
+
+    companion object {
+
+//        @ApplicationScope
+//        @Provides
+//        fun provideAppDao(
+//            application: Application
+//        ): AppDao {
+//            return AppDatabase.getInstance(application).appDao()
+//        }
+//
+//        @ApplicationScope
+//        @Provides
+//        fun provideApiClient(): FoodRecipeApiService {
+//            return FoodRecipeApiClient().client
+//        }
+    }
+}
---
+++ app/src/main/java/ru/internetcloud/foody4/di/ViewModelModule.kt
@@ -0,0 +1,18 @@
+package ru.internetcloud.foody4.di
+
+import dagger.Module
+
+@Module
+interface ViewModelModule {
+
+    // перечислить тут все вью-модели
+//    @IntoMap
+//    @ViewModelKey(FoodRecipeListViewModel::class)
+//    @Binds
+//    fun bindFoodRecipeListViewModel(impl: FoodRecipeListViewModel): ViewModel
+//
+//    @IntoMap
+//    @ViewModelKey(RecipesBottomSheetViewModel::class)
+//    @Binds
+//    fun bindRecipesBottomSheetViewModel(impl: RecipesBottomSheetViewModel): ViewModel
+}
